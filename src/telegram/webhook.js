01431839d9070a63a84c367485a9e5792d0a5250
Dmytro Borysovskyi <bbbara10@gmail.com>
2018-09-28 13:17:11 +0300

    WIP: docker

--- src/telegram/webhook.js
+++ src/telegram/webhook.js
@@ -1,3 +1,11 @@
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+;(async function () {
+  const {stdout,} = await exec(`npm run --prefix ${process.env.INIT_CWD} telegram:webhook:set:w`)
+  console.info(stdout)
+})()
+
 module.exports = (req, res, next) => {
   console.debug('Webhook called:', JSON.stringify(req.body))
   next()
